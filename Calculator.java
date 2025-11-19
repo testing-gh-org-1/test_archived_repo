5f8150643a2f3f7edccc058e9ba9ebd78a515939
thribhuvant <thribhuvana.t@digicert.com>
2025-11-19 14:46:34 +0530

    Sample code added

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+    
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        
+        System.out.println("Addition: 10 + 5 = " + calc.add(10, 5));
+        System.out.println("Subtraction: 10 - 5 = " + calc.subtract(10, 5));
+        System.out.println("Multiplication: 10 * 5 = " + calc.multiply(10, 5));
+        System.out.println("Division: 10 / 5 = " + calc.divide(10, 5));
+    }
+}
